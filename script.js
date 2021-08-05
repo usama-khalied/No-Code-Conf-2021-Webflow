0e26804bed66d87ebc9ff30dc0a1a28516322aea
Usama Khalied <usamakhalied@gmailcom>
2021-08-05 15:09:53 +0500

    laga reh

---
+++ script.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("scroll", () =>{
+document.getElementById("box1").style.transform.Translate = "(40deg)";
+    document.getElementById("box2").style.TransformOrigin = "0 0";
+
+
+})
